cb956237374dc7b229528741a25c3fff77b5c885
Jason Leach <jlspursfan@gmail.com>
2019-05-13 00:31:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-  return array.join(", ")
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    str = array[0] + " and " + array[1]
+    return str
+  else
+    
+  end
+
 end
